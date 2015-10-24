1ea5126a034c7252ca0aabe88e741eee64e640b2
kushal tirumala <kushaltirumala99@gmail.com>
2015-10-24 03:44:19 -0700

    ws dependency

--- index.js
+++ index.js
@@ -1,43 +1,12 @@
-var WebSocket = require('ws');
-var ws = new WebSocket('ws://localhost:7204/myo/1');
+var Myo = require('myo');
 
-var myoID;
+//Start talking with Myo Connect
+Myo.connect('com.example.musicApp');
 
-ws.on('message', function(message) {
-	json = JSON.parse(message);
-    
-    if (json[0] != "event")
-    	return console.log(message);
-
-    var data = json[1];
-
-    if (data.type == "connected") {
-    	myoID = data.myo;
-    }
-
-    if (data.type != "orientation") {
-        console.log(data)
-    }
+Myo.on('fist', function(){
+    console.log('Hello Myo!');
+    this.vibrate();
 });
-
-function requestVibrate() {
-    var data = {
-        "command": "vibrate",
-        "myo": myoID,
-        "type": "short"
-    }
-    console.log("Sending vibrate", JSON.stringify(data));
-    ws.send(JSON.stringify(data) + "\n");
-}
-
-function requestSignal() {
-	var data = {
-		"command": "request_rssi",
-		"myo": myoID
-	}
-    console.log("Sending request_rssi", JSON.stringify(data));
-    ws.send(JSON.stringify(data) + "\n");
-}
 
 setInterval(requestVibrate, 3000);
 setInterval(requestSignal, 3000);
