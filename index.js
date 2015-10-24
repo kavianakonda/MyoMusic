f933228937b0d8d8cc78e2a80907185bfbbdf6e3
kushal tirumala <kushaltirumala99@gmail.com>
2015-10-24 05:52:40 -0700

    howlinggg

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var Myo = require('myo');
+var howler = require('../resources/howler.js');
+
+
 
 //Start talking with Myo Connect
 Myo.connect('com.example.musicApp');
 
 Myo.on('fist', function(){
     console.log('wrap ur fist around my dick bb');
-    this.vibrate();
+    // this.vibrate();
+    this.trigger('beat-beat');
 });
 
 Myo.on('wave_in', function(){
@@ -37,19 +41,19 @@ Myo.on('disconnected', function() {
 	console.log('disconnected the Myo rip');
 });
 
-Myo.on('accelerometer', function(data){ 
-	if(data.x > 0) {
-		this.trigger('beat-beat');
-		//beat
-	}
-	else if (data.x < 0){
-		this.trigger('beat-high');
-		//tambourine
-	}
-});
+// Myo.on('accelerometer', function(data){ 
+// 	if(data.x > 0) {
+// 		this.trigger('beat-beat');
+// 		//beat
+// 	}
+// 	else if (data.x < 0){
+// 		this.trigger('beat-high');
+// 		//tambourine
+// 	}
+// });
 
 Myo.on('beat-beat', function() {
-	this.vibrate('long');
+    //strong beat
 
 })
 
