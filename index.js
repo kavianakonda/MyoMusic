d9a769fc0f74ea0fdcc7459066cbaa287469af72
alexmxu <alexmxu@outlook.com>
2015-10-24 07:05:57 -0400

    naughty myo mofo

--- index.js
+++ index.js
@@ -4,10 +4,31 @@ var Myo = require('myo');
 Myo.connect('com.example.musicApp');
 
 Myo.on('fist', function(){
-    console.log('Hello Myo!');
+    console.log('wrap ur fist around my dick bb');
+    this.vibrate();
+});
+
+Myo.on('wave_in', function(){
+    console.log('slap my booty honey');
+    this.vibrate();
+});
+
+Myo.on('wave_on', function(){
+    console.log('oooh backslap that ass bitch');
+    this.vibrate();
+});
+
+Myo.on('fingers_spread', function(){
+    console.log('sticky cum on dese fings');
+    this.vibrate();
+});
+
+Myo.on('double_tap', function(){
+    console.log('mmm... lemme see dat tounge');
     this.vibrate();
 });
 
 Myo.on('connected', function(){
     console.log('connected!', this.id)
+    this.vibrate('long');
 });
