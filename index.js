1ec160be0dff6771df7507fbc7c28007f2f14c33
alexmxu <alexmxu@outlook.com>
2015-10-24 13:56:23 -0400

    2 songs

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ var Myo = require('myo');
 var Player = require('player');
 
 var beat = new Player('sounds/uptown.mp3');
+var beat2 = new Player('sounds/piano.mp3')
 
 //Start talking with Myo Connect
 Myo.connect('com.example.musicApp');
 
+var ifFirst = 0;
 Myo.on('fist', function(){
-    console.log('wrap ur fist around my dick bb');
-    // this.vibrate();
-    this.trigger('beat-beat');
+    if (ifFirst == 0){
+        console.log('wrap ur fist around my dick bb');
+        // this.vibrate();
+        this.trigger('beat-beat');
+        ifFirst = 1;
+    }
+    else {
+        this.trigger('beat-beat2');
+    }
 });
 
 Myo.on('wave_in', function(){
@@ -20,7 +28,7 @@ Myo.on('wave_in', function(){
 Myo.on('wave_on', function(){
     console.log('oooh backslap that ass bitch');
     this.vibrate();
-});
+});5
 
 Myo.on('fingers_spread', function(){
     console.log('sticky cum on dese fings');
@@ -55,6 +63,10 @@ Myo.on('disconnected', function() {
 Myo.on('beat-beat', function() {
     //strong beat
     beat.play();
+});
+
+Myo.on('beat-beat2', function(){
+    beat2.play();
 })
 
 Myo.on('beat-high', function() {
